2b4d9dc7e51db94a57598b79528e59722b0a3521
zhangyue <1123842128@qq.com>
2018-07-29 17:29:31 +0800

    updata orm

--- www/orm.py
+++ www/orm.py
@@ -17,7 +17,7 @@ def log(sql, args=()):
 
 '''创造连接池'''
 
-
+# **kw : [python学习笔记 可变参数关键字参数**kw相关学习]:https://www.cnblogs.com/Commence/p/5578215.html
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     # 全局变量，使用global声明
@@ -94,17 +94,24 @@ def create_arg_string(num):
 
 
 class Field(object):
+    # __init__作用是初始化已实例化后的对象。
+    # 子类可以不重写__init__，实例化子类时，会自动调用超类中已定义的__init__
+    # 但如果重写了__init__，实例化子类时，则不会隐式的再去调用超类中已定义的__init__，会报错
+    # 所以如果重写了__init__，为了能使用或扩展超类中的行为，最好显式的调用超类的__init__方法
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
         self.primary_key = primary_key
         self.default = default
 
+    # 在python中方法名如果是__xxxx__()的，那么就有特殊的功能，因此叫做“魔法”方法
+    # 当使用print输出对象的时候，只要自己定义了__str__(self)方法，那么就会打印从在这个方法中return的数据
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 
 class StringField(Field):
+    '''super ?????????继承?????????'''
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super.__init__(name, ddl, primary_key, default)
 
@@ -131,7 +138,6 @@ class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
-'''
 class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
         if name == 'Model':
@@ -150,5 +156,69 @@ class ModelMetaclass(type):
             if isinstance(v, Field):
                 logging.info('  found mapping: %s ==> %s' % (k, v))
                 mapings[k] = v
-                if v.primary_key
+                if v.primary_key:
+                    if primaryKey:
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise StandarError('Primary key not found.')
+        for k in mapings.keys():
+            # pop() 函数用于移除列表中的一个元素（默认最后一个元素），并且返回该元素的值。
+            attrs.pop(k)
+        # lambda 函數：
+        # 冒号左边→想要传递的参数
+        # 冒号右边→想要得到的数（可能带表达式）
+        escaped_fields = list(map(lambda f:'%s' % f, fields))
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey  # 主键属性名
+        attrs['__fields__'] = fields  # 除主键外的属性名
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+        tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+        tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefualt(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self._mappings_[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' % (key, str(value)))
+                setattr(self, key, value)
+        return  value
 '''
+    @classmethod
+    async def findAll(cls, where = None, args = None, **kw):
+        'find objects by where clause.'
+        sql = [cls._select_]
+        if where :
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+
+'''
+
