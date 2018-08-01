96ba883c5558bf6add4888569f586023c8ba62a5
zhangyue <1123842128@qq.com>
2018-08-01 14:57:17 +0800

    add model

---
+++ www/model.py
@@ -0,0 +1,46 @@
+#!user/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+models for user, blog, comment.
+'''
+# uuid是什么
+import time, uuid
+
+from orm import Model, StringField, BooleanField,FloatField, TextField
+
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+
+class User(Model):
+    _table_ = 'users'
+    id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
+    email = StringField(ddl = 'varchar(50)')
+    passwd = StringField(ddl = 'varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl = 'varchar(50)')
+    image = StringField(ddl = 'varchar(500)')
+    create_at = FloatField(default = time.time)
+
+class Blog(Model):
+    _table_ = 'blogs'
+    id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
+    user_id = StringField(ddl = 'varchar(50)')
+    user_name = StringField(ddl = 'varchar(50)')
+    user_image = StringField(ddl = 'varchar(500)')
+    name = StringField(ddl = 'varchar(50)')
+    summary = StringField(ddl = 'varchar(200)')
+    content = TextField()
+    create_at = FloatField(default = time.time)
+
+class Comment(Model):
+    _table_ = 'comments'
+    id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
+    blog_id = StringField(ddl = 'varchar(50)')
+    user_id = StringField(ddl = 'varchar(50)')
+    user_name = StringField(ddl = 'varchar(50)')
+    user_image = StringField(ddl = 'varchar(500)')
+    content = TextField()
+    create_at = FloatField(default = time.time)
